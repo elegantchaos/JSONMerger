0d790172382b62f5c3a55f7a8889b5cbafd9241c
Sam Deane <sam@elegantchaos.com>
2025-10-31 15:30:30 +0000

    copying/moving working

--- Sources/JSONMerge/OverlayCommand.swift
+++ Sources/JSONMerge/OverlayCommand.swift
@@ -42,15 +42,81 @@ struct OverlayCommand: ParsableCommand {
     let configURL = url.appending(path: "config.json")
     let config = try decoder.decode(OverlayConfig.self, from: Data(contentsOf: configURL))
 
-    let o = outputURL.appending(path: config.output)
-    try? FileManager.default.createDirectory(
-      at: o.deletingLastPathComponent(), withIntermediateDirectories: true)
-    try "TBC".write(to: o, atomically: true, encoding: .utf8)
-    print("overlaying \(url.path)")
+    for stage in config.stages {
+      if let copyConfig = stage.copy {
+        try copy(copyConfig, at: url, to: outputURL)
+      }
+
+      if let moveConfig = stage.move {
+        try move(moveConfig, at: url, to: outputURL)
+      }
+
+      if let mergeConfig = stage.merge {
+        try merge(mergeConfig, at: url, to: outputURL)
+      }
+    }
+  }
+
+  func move(_ config: GroupConfig, at inputRoot: URL, to outputRoot: URL) throws {
+    for input in config.from {
+      let inputURL = inputRoot.appending(path: input)
+      let outputURL = outputRoot.appending(path: config.to).appending(
+        path: inputURL.lastPathComponent)
+      try inputURL.copy(to: outputURL)
+    }
+  }
+
+  func copy(_ config: CopyConfig, at root: URL, to outputURL: URL) throws {
+    let inputURL = root.appending(path: config.from)
+    let destination = outputURL.appending(path: config.to)
+    try inputURL.copy(to: destination)
+  }
+
+  func merge(_ config: GroupConfig, at root: URL, to outputURL: URL) throws {
+    let inputs = config.from.compactMap {
+      try? JSONFile(contentsOf: root.appending(path: "\($0).json"))
+    }
+    let merger = JSONMerger(options: .init(uniqueLists: true, verbose: verbose))
+    let merged = try merger.merge(inputs)
+    let mergedURL = outputURL.appending(path: config.to)
+    try merged.formatted.write(to: mergedURL)
   }
 }
 
+extension String {
+  func write(to url: URL) throws {
+    try? FileManager.default.createDirectory(
+      at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
+    try self.write(to: url, atomically: true, encoding: .utf8)
+  }
+}
+
+extension URL {
+  func copy(to destination: URL) throws {
+    let fm = FileManager.default
+    try? fm.createDirectory(
+      at: destination.deletingLastPathComponent(), withIntermediateDirectories: true)
+    try? fm.removeItem(at: destination)
+    try fm.copyItem(at: self, to: destination)
+  }
+}
+
+struct GroupConfig: Codable {
+  let from: [String]
+  let to: String
+}
+
 struct OverlayConfig: Codable {
-  let inputs: [String]
-  let output: String
+  let stages: [OverlayStage]
+}
+
+struct CopyConfig: Codable {
+  let from: String
+  let to: String
+}
+
+struct OverlayStage: Codable {
+  let copy: CopyConfig?
+  let move: GroupConfig?
+  let merge: GroupConfig?
 }
