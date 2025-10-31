79d309d5baf1d575433158d6230145685b7a565a
Sam Deane <sam@elegantchaos.com>
2025-10-31 11:27:34 +0000

    renamed executable

--- Package.swift
+++ Package.swift
@@ -1,29 +1,44 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 31/10/2025.
+//  All code (c) 2025 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// swift-tools-version:6.2
 
 import PackageDescription
 
 let package = Package(
-    name: "JSONMerge",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(url: "https://github.com/elegantchaos/Matchable.git", from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "JSONMerge",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ]
-        ),
-        .testTarget(
-            name: "JSONMergeTests",
-            dependencies: [
-                "JSONMerge",
-                .product(name: "Matchable", package: "Matchable"),
-            ]
-        ),
-    ]
+  name: "JSONMerge",
+
+  platforms: [
+    .macOS(.v15)
+  ],
+
+  products: [
+    .executable(
+      name: "json-merge",
+      targets: ["JSONMerge"]
+    )
+  ],
+
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    .package(url: "https://github.com/elegantchaos/Matchable.git", from: "1.0.0"),
+  ],
+
+  targets: [
+    .executableTarget(
+      name: "JSONMerge",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ]
+    ),
+    .testTarget(
+      name: "JSONMergeTests",
+      dependencies: [
+        "JSONMerge",
+        .product(name: "Matchable", package: "Matchable"),
+      ]
+    ),
+  ]
 )
